9ae8934d335305a953bfd76fa6e506bdb306a4ff
Andreone70 <tufanariandrea1@gmail.com>
2022-12-13 11:55:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-print ("ciao")
-print("peppe")
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run("0.0.0.0")
